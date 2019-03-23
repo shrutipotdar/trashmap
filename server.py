5c960d67c98f11248a32e4c9155809c689b9f303
shruti <shrutipotdar99@gmail.com>
2019-03-24 04:31:17 +0530

    nc

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ app = Bottle(__name__)
 @app.route('/')
 def root():
     
-    return static_file('index.html', root='templates)
+    return static_file('index.html', root='templates')
 
 
 # Static Routes
