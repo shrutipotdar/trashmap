27fa07d5293f3559238d4128bd3d3787b46c04c0
shruti <shrutipotdar99@gmail.com>
2019-03-24 04:25:48 +0530

    nc

---
+++ server.py
@@ -0,0 +1,45 @@
+from bottle import Bottle, run, route, static_file, request, response, template, redirect
+import json
+import time
+import os
+
+
+app = Bottle(__name__)
+
+
+@app.route('/')
+def root():
+    
+    return static_file('index.html', root='templates)
+
+
+# Static Routes
+@app.route('/<filename:re:.*\.html>')
+def javascripts(filename):
+    return static_file(filename, root='templates')
+
+@app.route('/<filename:re:.*\.js>')
+def javascripts(filename):
+    return static_file(filename, root='static')
+
+@app.route('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='static')
+
+@app.route('/<filename:re:.*\.(jpg|png|gif|ico|svg)>')
+def images(filename):
+    return static_file(filename, root='static')
+
+@app.route('/<filename:re:.*\.(eot|ttf|woff|svg)>')
+def fonts(filename):
+    return static_file(filename, root='static')
+
+@app.route('/<filename:re:.*\.html>')
+def javascripts(filename):
+    return static_file(filename, root='static')
+
+@app.hook('after_request')
+def enable_cors():
+	response.headers['Access-Control-Allow-Origin'] = '*'
+	response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+	response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
